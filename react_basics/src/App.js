66a812ff39dd6ce5125cafcd66fe5554ebc80600
Rapha-Y <raph.yuki@gmail.com>
2020-07-22 15:50:33 -0300

    added css to todo list

--- react_basics/src/App.js
+++ react_basics/src/App.js
@@ -1,38 +1,14 @@
 import React from 'react'
 
+import TodoItem from './components/TodoItem.js'
+
 function App() {
-    const date = new Date()
-    const hours = date.getHours()
-    let timeOfDay
-
-    const styles = {
-        fontSize: 30
-    }
-
-    if (hours < 12) {
-        timeOfDay = "morning"
-        styles.color = "pink"
-    } else if (hours < 17) {
-        timeOfDay = "afternoon"
-        styles.color = "red"
-    } else {
-        timeOfDay = "night"
-        styles.color = "black"
-    }
-
     return(
-        <div>
-            <div style={styles}>Good {timeOfDay}</div><br />
-            <br />
-
-            <input type="checkbox"></input>
-            <span> Carrots</span><br />
-            <input type="checkbox"></input>
-            <span> Potatoes</span><br />
-            <input type="checkbox"></input>
-            <span className="green"> Asparaguses</span><br />
-            <input type="checkbox"></input>
-            <span> Onions</span><br />
+        <div className="todo-list">
+            <TodoItem />
+            <TodoItem />
+            <TodoItem />
+            <TodoItem />
         </div>
     )
 }
---
+++ react_basics/src/components/TodoItem.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function TodoItem() {
+    return (
+        <div className="todo-item">
+            <input type="checkbox"></input>
+            <span>Item</span>
+        </div>
+    )
+}
+
+export default TodoItem
