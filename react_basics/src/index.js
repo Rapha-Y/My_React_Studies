e8ce5171d0f9e6ff1afe0aa2b4753260590a48d9
Rapha-Y <raph.yuki@gmail.com>
2020-07-22 12:25:05 -0300

    modified the displayed component's html elements

--- react_basics/src/index.js
+++ react_basics/src/index.js
@@ -1,17 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-function MyApp() {
+function MyInfo() {
   return( 
-    <ul>
-      <li>Sword</li>
-      <li>Lance</li>
-      <li>Axe</li>
-    </ul>
+    <div>
+      <h1>Emily Yuki</h1>
+      <p>Into medieval fantasy RPGs, recently got into a card game.</p>
+      <ul>
+        <li>The US - To visit a friend</li>
+        <li>Canada - Heard ppl were polite there.</li>
+        <li>Akihabara - But I'd need money to spend, I suppose.</li>
+      </ul>
+    </div>
   ) 
 }
 
 ReactDOM.render(
-  <MyApp />,
+  <MyInfo />,
   document.getElementById("root")
 )
