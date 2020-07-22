51d95485d3bc33112601a661c9f94e782ed3d0da
Rapha-Y <raph.yuki@gmail.com>
2020-07-22 12:30:46 -0300

    moved the displayed component onto a new file

---
+++ react_basics/src/components/MyInfo.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function MyInfo() {
+    return( 
+        <div>
+        <h1>Emily Yuki</h1>
+        <p>Into medieval fantasy RPGs, recently got into a card game.</p>
+        <ul>
+            <li>The US - To visit a friend</li>
+            <li>Canada - Heard ppl were polite there.</li>
+            <li>Akihabara - But I'd need money to spend, I suppose.</li>
+        </ul>
+        </div>
+    ) 
+}
+
+export default MyInfo
--- react_basics/src/index.js
+++ react_basics/src/index.js
@@ -1,19 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-function MyInfo() {
-  return( 
-    <div>
-      <h1>Emily Yuki</h1>
-      <p>Into medieval fantasy RPGs, recently got into a card game.</p>
-      <ul>
-        <li>The US - To visit a friend</li>
-        <li>Canada - Heard ppl were polite there.</li>
-        <li>Akihabara - But I'd need money to spend, I suppose.</li>
-      </ul>
-    </div>
-  ) 
-}
+import MyInfo from './components/MyInfo.js'
 
 ReactDOM.render(
   <MyInfo />,
