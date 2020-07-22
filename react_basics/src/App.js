7a0ba119b4c104cf940a78315abd376303149126
Rapha-Y <raph.yuki@gmail.com>
2020-07-22 15:20:25 -0300

    added js variables to jsx code

--- react_basics/src/App.js
+++ react_basics/src/App.js
@@ -1,8 +1,16 @@
 import React from 'react'
 
 function App() {
+    const abc = "a b c"
+    const def = "d e f"
+    const date = new Date()
+
     return(
         <div>
+            {`${abc} ${def}`}<br />
+            {date.getHours()}<br />
+            <br />
+
             <input type="checkbox"></input>
             <span> Carrots</span><br />
             <input type="checkbox"></input>
