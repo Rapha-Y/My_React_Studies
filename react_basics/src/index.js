fa287fee18a57a27820d64534ae25f1ea692064b
Rapha-Y <raph.yuki@gmail.com>
2020-07-22 12:13:38 -0300

    put the html into a functional component

--- react_basics/src/index.js
+++ react_basics/src/index.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+function MyApp() {
+  return( 
+    <ul>
+      <li>Sword</li>
+      <li>Lance</li>
+      <li>Axe</li>
+    </ul>
+  ) 
+}
+
 ReactDOM.render(
-  <ul>
-    <li>Sword</li>
-    <li>Lance</li>
-    <li>Axe</li>
-  </ul>, 
+  <MyApp />,
   document.getElementById("root")
 )
