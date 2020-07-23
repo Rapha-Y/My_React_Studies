ccfed0ea955a3fe0a0d70d177b00dbfddbdd7257
Rapha-Y <raph.yuki@gmail.com>
2020-07-23 13:40:16 -0300

    added state onto the app component

--- react_basics/src/App.js
+++ react_basics/src/App.js
@@ -1,42 +1,18 @@
 import React from 'react'
 
 class App extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            statement: "Ain't a real place."
+        }
+    }
+    
     render() {
         return (
             <div>
-                <Header username="Anni" />
-                <Greeting />
+                <h1>Split States of Europe. {this.state.statement}</h1>
             </div>
-        )
-    }
-}
-
-class Header extends React.Component {
-    render() {
-        return (
-            <header>
-                <p>Welcome, {this.props.username}!</p>
-            </header>
-        )
-    }
-}
-
-class Greeting extends React.Component {
-    render() {
-        const date = new Date()
-        const hours = date.getHours()
-        let timeOfDay
-
-        if (hours < 12) {
-            timeOfDay = "morning"
-        } else if (hours < 17) {
-            timeOfDay = "afternoon"
-        } else {
-            timeOfDay = "night"
-        }
-
-        return (
-            <h1>Good {timeOfDay} to you, pal!</h1>
         )
     }
 }
