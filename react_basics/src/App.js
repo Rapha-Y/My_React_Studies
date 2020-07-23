cf357fabf1eca79a2456e536e3706521b795383e
Rapha-Y <raph.yuki@gmail.com>
2020-07-23 12:20:19 -0300

    added component mapping

--- react_basics/src/App.js
+++ react_basics/src/App.js
@@ -2,27 +2,18 @@ import React from 'react'
 
 //import TodoItem from './components/TodoItem.js'
 import Joke from './components/Joke'
+import jokesData from './data/jokesData'
 
 function App() {
+    const jokeComponents = jokesData.map((joke) => {
+        return (
+            <Joke key={joke.id} question={joke.question} punchline={joke.punchline} />
+        )
+    })
+
     return(
         <div>
-            <Joke 
-                question="What's the leading cause of divorce in long-term marriages?"
-                punchline="A stalemate."
-            />
-            <Joke 
-                question="You know what's not right?"
-                punchline="Left."
-            />
-            <Joke 
-                punchline="I'm glad I know sign language. It's become quite handy."
-            />
-            <Joke 
-                punchline="Those fish were shy. They were obviously coy."
-            />
-            <Joke 
-                punchline="I didn't have the faintest idea... as to why I passed out."
-            />
+            {jokeComponents}
         </div>
         /*<div className="todo-list">
             <TodoItem />
