b557909c51139e08d75cff4abc54c912bf25987c
Rapha-Y <raph.yuki@gmail.com>
2020-07-23 13:20:04 -0300

    added class-based components at App.js

--- react_basics/src/App.js
+++ react_basics/src/App.js
@@ -4,8 +4,39 @@ class App extends React.Component {
     render() {
         return (
             <div>
-                <h1>I'm inside a class-based component</h1>
+                <Header username="Anni" />
+                <Greeting />
             </div>
+        )
+    }
+}
+
+class Header extends React.Component {
+    render() {
+        return (
+            <header>
+                <p>Welcome, {this.props.username}!</p>
+            </header>
+        )
+    }
+}
+
+class Greeting extends React.Component {
+    render() {
+        const date = new Date()
+        const hours = date.getHours()
+        let timeOfDay
+
+        if (hours < 12) {
+            timeOfDay = "morning"
+        } else if (hours < 17) {
+            timeOfDay = "afternoon"
+        } else {
+            timeOfDay = "night"
+        }
+
+        return (
+            <h1>Good {timeOfDay} to you, pal!</h1>
         )
     }
 }
