8570cbecfa1e29036afce41c6c8c194e03b83436
Rapha-Y <raph.yuki@gmail.com>
2020-07-26 14:02:38 -0300

    added forms and destructuring examples

--- react_basics/src/App.js
+++ react_basics/src/App.js
@@ -4,33 +4,40 @@ class App extends React.Component {
     constructor() {
         super()
         this.state = {
-            loading: false,
-            character: {}
+            firstName: "",
+            lastName: ""
         }
+        this.handleChange = this.handleChange.bind(this)
     }
-    
-    componentDidMount() {
-        this.setState({
-            loading: true
-        })
 
-        fetch("https://swapi.dev/api/people/1")
-            .then(response => response.json())
-            .then(data => {
-                this.setState({
-                    loading: false,
-                    character: data
-                })
-            })
+    handleChange(event) {
+        const {name, value} = event.target
+        this.setState({
+            [name]: value
+        })
     }
 
     render() {
-        let txtMessage = this.state.loading ? "Loading..." : this.state.character.name
-
-        return(
-            <div>
-                {txtMessage}
-            </div>
+        return (
+            <form>
+                <input 
+                    type="text"
+                    value={this.state.firstName} 
+                    name="firstName" 
+                    placeholder="First Name" 
+                    onChange={this.handleChange} 
+                />
+                <br />
+                <input 
+                    type="text" 
+                    value={this.state.lastName}
+                    name="lastName" 
+                    placeholder="Last Name" 
+                    onChange={this.handleChange} 
+                />
+                <br />
+                <h1>{this.state.firstName} {this.state.lastName}</h1>
+            </form>
         )
     }
 }
