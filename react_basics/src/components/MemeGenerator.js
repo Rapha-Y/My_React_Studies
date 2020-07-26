97ebe33282cf16a6bcc706450577e3237b9447f5
Rapha-Y <raph.yuki@gmail.com>
2020-07-26 18:33:30 -0300

    added core components

---
+++ react_basics/src/components/MemeGenerator.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+class MemeGenerator extends React.Component {
+    constructor() {
+        super()
+        this.state = {}
+    }
+    
+    render() {
+        return (
+            <h1>MemeGenerator</h1>
+        )
+    }
+}
+
+export default MemeGenerator
