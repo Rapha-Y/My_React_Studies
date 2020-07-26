52ccb16a3dcdf5dc47441f1897c67d8245de3b4f
Rapha-Y <raph.yuki@gmail.com>
2020-07-26 16:44:14 -0300

    added forms based on previously explored examples

--- react_basics/src/App.js
+++ react_basics/src/App.js
@@ -6,9 +6,15 @@ class App extends React.Component {
         this.state = {
             firstName: "",
             lastName: "",
-            isEvil: false,
+            age: "",
             gender: "",
-            favFood: "Udon"
+            destination: "",
+            peanuts: false,
+            milk: false,
+            egg: false,
+            wheat: false,
+            fish: false,
+            shellfish: false
         }
         this.handleChange = this.handleChange.bind(this)
     }
@@ -24,84 +30,176 @@ class App extends React.Component {
         })
     }
 
+    renderDiet() {
+        const restrictions = ["peanuts", "milk", "egg", "wheat", "fish", "shellfish"]
+        const checks = [
+            this.state.peanuts,
+            this.state.milk,
+            this.state.egg,
+            this.state.wheat,
+            this.state.fish,
+            this.state.shellfish
+        ]
+
+        let output = ""
+        
+        for (let i = 0; i < restrictions.length; i++) {
+            if (checks[i]) {
+                if (output.length === 0) {
+                    let firstWord = restrictions[i]
+                    firstWord = firstWord[0].toUpperCase() + firstWord.slice(1)
+                    output = output.concat(firstWord)
+                } else {
+                    output = output.concat(", ")
+                    output = output.concat(restrictions[i])
+                }
+            }
+        }
+
+        return output
+    }
+
     render() {
         return (
-            <form>
-                <input 
-                    type="text"
-                    value={this.state.firstName} 
-                    name="firstName" 
-                    placeholder="First Name" 
-                    onChange={this.handleChange} 
-                />
-                <br />
-                
-                <input 
-                    type="text" 
-                    value={this.state.lastName}
-                    name="lastName" 
-                    placeholder="Last Name" 
-                    onChange={this.handleChange} 
-                />
-                <br />
-
-                <textarea value={"Lorem ipsum"} onChange={this.handleChange} />
-                <br />
-
-                <label>
+            <main>
+                <form>
                     <input 
-                        type="checkbox" 
-                        name="isEvil" 
-                        checked={this.state.isEvil} 
+                        placeholder="First Name" 
+                        value={this.state.firstName}
+                        onChange={this.handleChange}
+                        type="text"
+                        name="firstName" 
+                    /><br />
+                    <input 
+                        placeholder="Last Name" 
+                        value={this.state.lastName} 
                         onChange={this.handleChange} 
-                    />
-                    Is evil?
-                </label>
-                <br />
-
-                <label>
+                        type="text"
+                        name="lastName"
+                    /><br />
                     <input 
-                        type="radio"
-                        name="gender"
-                        value="female"
-                        checked={this.state.gender === "female"}
-                        onChange={this.handleChange}
-                    />
-                    Female
-                </label>
-                <label>
-                    <input 
-                        type="radio"
-                        name="gender"
-                        value="male"
-                        checked={this.state.gender === "male"}
-                        onChange={this.handleChange}
-                    />
-                    Male
-                </label>
-                <label>
-                    <input 
-                        type="radio"
-                        name="gender"
-                        value="non-binary"
-                        checked={this.state.gender === "non-binary"}
-                        onChange={this.handleChange}
-                    />
-                    Non-binary
-                </label>
-                <br />
+                        placeholder="Age" 
+                        value={this.state.age}
+                        onChange={this.handleChange} 
+                        type="text"
+                        name="age"
+                    /><br />
 
-                <select value={this.state.favFood} onChange={this.handleChange} name="favFood">
-                    <option value="">Udon</option>
-                    <option value="">Spaghetti</option>
-                    <option value="">Ramen</option>
-                </select>
-                <br />
+                    <label>
+                        <input
+                            value="Female"
+                            onChange={this.handleChange}
+                            type="radio"
+                            name="gender"
+                            checked={this.state.gender === "Female"}
+                        />
+                        Female
+                    </label>
+                    <label>
+                        <input
+                            value="Male"
+                            onChange={this.handleChange}
+                            type="radio"
+                            name="gender"
+                            checked={this.state.gender === "Male"}
+                        />
+                        Male
+                    </label>
+                    <label>
+                        <input
+                            value="Other"
+                            onChange={this.handleChange}
+                            type="radio"
+                            name="gender"
+                            checked={this.state.gender === "Other"}
+                        />
+                        Other
+                    </label>
+                    <br />
 
-                <h1>{this.state.firstName} {this.state.lastName}</h1>
-                <h2>You are {this.state.gender}</h2>
-                <h2>Your favorite food is {this.state.favFood}</h2>
-            </form>
+                    <select
+                        value={this.state.destination} 
+                        onChange={this.handleChange}
+                        name="destination"
+                    >
+                        <option value="">Choose a continent</option>
+                        <option value="Africa">Africa</option>
+                        <option value="Antarctica">Antarctica</option>
+                        <option value="America">America</option>
+                        <option value="Asia">Asia</option>
+                        <option value="Europe">Europe</option>
+                        <option value="Oceania">Oceania</option>
+                    </select>
+                    <br />
+
+                    <label>
+                        <input
+                            onChange={this.handleChange}
+                            type="checkbox"
+                            name="peanuts"
+                            checked={this.state.peanuts}
+                        />
+                        Peanut
+                    </label>
+                    <label>
+                        <input
+                            onChange={this.handleChange}
+                            type="checkbox"
+                            name="milk"
+                            checked={this.state.milk}
+                        />
+                        Milk
+                    </label>
+                    <label>
+                        <input
+                            onChange={this.handleChange}
+                            type="checkbox"
+                            name="egg"
+                            checked={this.state.egg}
+                        />
+                        Egg
+                    </label>
+                    <label>
+                        <input
+                            onChange={this.handleChange}
+                            type="checkbox"
+                            name="wheat"
+                            checked={this.state.wheat}
+                        />
+                        Wheat
+                    </label>
+                    <label>
+                        <input
+                            onChange={this.handleChange}
+                            type="checkbox"
+                            name="fish"
+                            checked={this.state.fish}
+                        />
+                        Fish
+                    </label>
+                    <label>
+                        <input
+                            onChange={this.handleChange}
+                            type="checkbox"
+                            name="shellfish"
+                            checked={this.state.shellfish}
+                        />
+                        Shellfish
+                    </label>
+                    <br />
+
+                    <button>Submit</button>
+                </form>
+                <hr />
+                <h2>Entered information:</h2>
+                <p>Your name: {this.state.firstName} {this.state.lastName}</p>
+                <p>Your age: {this.state.age}</p>
+                <p>Your gender: {this.state.gender}</p>
+                <p>Your destination: {this.state.destination}</p>
+                <p>
+                    Your dietary restrictions: {this.renderDiet()}
+                </p>
+            </main>
         )
     }
 }
