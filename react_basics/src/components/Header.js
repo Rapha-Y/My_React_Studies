d901e2d75602a2e5070ed755023a9a5b91e24a81
Rapha-Y <raph.yuki@gmail.com>
2020-07-26 18:57:25 -0300

    added api call to retrieve data

--- react_basics/src/components/Header.js
+++ react_basics/src/components/Header.js
@@ -2,9 +2,13 @@ import React from 'react'
 
 function Header() {
     return (
-        <h1>
-            Header
-        </h1>
+        <header>
+            <img 
+                src="https://i.kym-cdn.com/entries/icons/medium/000/027/475/Screen_Shot_2018-10-25_at_11.02.15_AM.png" 
+                alt="Surprised Pikachu face"    
+            />
+            <p>Meme Generator</p>
+        </header>
     )
 }
 
--- react_basics/src/components/MemeGenerator.js
+++ react_basics/src/components/MemeGenerator.js
@@ -3,7 +3,22 @@ import React from 'react'
 class MemeGenerator extends React.Component {
     constructor() {
         super()
-        this.state = {}
+        this.state = {
+            topText: "",
+            bottomText: "",
+            randomImage: "https://i.kym-cdn.com/entries/icons/medium/000/026/489/crying.jpg",
+            allMemeImgs: []
+        }
+    }
+
+    componentDidMount() {
+        fetch("https://api.imgflip.com/get_memes")
+            .then(res => res.json())
+            .then(response => {
+                this.setState({
+                    allMemeImgs: response.data.memes
+                })
+            })
     }
     
     render() {
